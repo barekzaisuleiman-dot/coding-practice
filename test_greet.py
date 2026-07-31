d95949b1407a22a5b5125eb31d8f17450c4d8f69
Suleiman Barekzai <barekzaisuleiman@gmail.com>
2026-07-31 11:48:36 -0400

    Add missing test coverage for shout()

--- test_greet.py
+++ test_greet.py
@@ -1,5 +1,9 @@
-from greet import greet
+from greet import greet, shout
 
 
 def test_greet():
     assert greet("world") == "Hello, world!"
+
+
+def test_shout():
+    assert shout("this is fine") == "THIS IS FINE!"
